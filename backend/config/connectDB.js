c4df99f63b7715071c52ea89eed1da42582ea208
ruthreshjp <ruthreshprabhakaran@gmail.com>
2025-12-24 21:24:10 +0530

    deployment update

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/tour-app";
+    const MONGO_URI =
+      process.env.MONGO_URI ||
+      "mongodb+srv://Ruthresh:rhwKOIbwt76jBcYX@travelapp.6ejy1ox.mongodb.net/?appName=travelapp";
     await mongoose.connect(MONGO_URI);
     console.log("MongoDB Connected");
   } catch (error) {
